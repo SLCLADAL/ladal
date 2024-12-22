8e81316eeca4370c5f4ef93e3b7b9c9e605ee6ff
Laurence Anthony <laurenceanthony@users.noreply.github.com>
2024-12-23 09:53:32 +1000

    rendered and changed gitignore

--- render.sh
+++ render.sh
@@ -5,6 +5,24 @@ PARENT_DIR="."
 
 echo "$(realpath "$PARENT_DIR")"
 
+runall=true
+usecache=true
+usethis="postag"
+
+# the following tutorials need to first be built without cache (no idea why!)
+# regression, postag
+
+if $runall && $usecache; then
+    echo "Running all tasks (CACHE)."
+    quarto render --execute-dir $PARENT_DIR > logs/main.log 2>&1
+    exit 0
+elif $runall; then
+    echo "Running all tasks (NO CACHE)."
+    quarto render --no-cache --execute-dir $PARENT_DIR > logs/main.log 2>&1
+    exit 0
+fi
+
+
 # Set the start index (change this to where you want to start)
 START_INDEX=0
 
@@ -13,6 +31,7 @@ STOP_INDEX=-1
 
 # Loop through all subfolders in the tutorials folder
 count=0  # Folder counter
+
 for folder in "$PARENT_DIR/tutorials"/*/; do
   # Check if it's a directory (just a safety check)
   if [ -d "$folder" ]; then
@@ -31,22 +50,21 @@ for folder in "$PARENT_DIR/tutorials"/*/; do
     # Optional: You can add additional skipping logic here, like checking folder names
     # Example: Skip folders named "skip_this_folder"
     folder_name=$(basename "$folder")
-    if [ "$folder_name" != "regression" ]; then
+    if [ "$folder_name" != "$usethis" ]; then
       # echo "Skipping folder: $folder_name"
       continue
     fi
     
-    echo "$(realpath "$folder")"
+    if $usecache; then
+        # echo "Rendering $(realpath "$folder") with cache."
+        echo "Rendering "$folder_name" (CACHE)."
+        quarto render tutorials/${folder_name} --execute-dir $PARENT_DIR > logs/log_${folder_name}.log 2>&1
+    else
+        echo "Rendering "$folder_name" (NO CACHE)."
+        quarto render tutorials/${folder_name} --no-cache --execute-dir $PARENT_DIR > logs/log_${folder_name}.log 2>&1
+    fi
 
-    # Render the Quarto project with --no-cache
-    echo "Rendering folder $count: $folder_name"
-    # quarto render "$folder" --no-cache --execute-dir "$(realpath "$PARENT_DIR")"
-    # quarto render tutorials/${folder_name} --no-cache --execute-dir /Users/laurenceanthony/Documents/projects/@projects_misc/LADALQ_TEMP > logs/log_${folder_name}.log 2>&1
-    quarto render tutorials/${folder_name} --no-cache --execute-dir /Users/laurenceanthony/Documents/projects/@projects_misc/LADALQ_TEMP > logs/log_${folder_name}.log 2>&1
-    # quarto render  --quiet --no-cache --execute-dir  /Users/laurenceanthony/Documents/projects/@projects_misc/LADALQ_TEMP
-    # break
     # Increment the counter
     ((count++))
-  fi
-  
+    fi
 done
